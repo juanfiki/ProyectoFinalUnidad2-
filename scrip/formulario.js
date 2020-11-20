3352efd62d60ba3ca57e670097d30be0d6357220
juanfiki <juanfiquitiva@hotmail.com>
2020-11-20 16:34:31 -0500

    js de la pag

---
+++ scrip/formulario.js
@@ -0,0 +1,13 @@
+
+var correo = document.getElementById('correo');
+
+
+function enviarFomulario (){
+   console.log('envio')
+   // alert("Ayrton Senna")
+   if( correo.value.length <=0 ){
+       swal("Ingrese todos los Datos","Correo");
+   }else{
+   
+   }
+}
---
+++ scrip/jquery.js
@@ -0,0 +1,13 @@
+//  $(document).ready(function() {
+//      document.write("conectados")
+//        })
+
+$(".logo").ready(function(){
+     var div = $(".logo");
+     div.animate({height: '300px', opacity: '0.4'}, "slow");
+     div.animate({width: '500px', opacity: '0.8'}, "slow");
+     div.animate({height: '150px', opacity: '0.4'}, "slow");
+     div.animate({width: '500px', opacity: '0.8'}, "slow");
+   });
+
+   
